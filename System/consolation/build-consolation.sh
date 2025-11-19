f2678b7efe55905dff161fc984d136218d478556
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-19 20:50:22 +0000

    Added consolation

---
+++ System/consolation/build-consolation.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+PRETTY_NAME=consolation
+MAJOR=0
+MINOR=0
+PATCH=10
+VERSION=0.0.10
+
+if [ ! -f $0 ]; then return; fi
+
+mkdir temporary-destdir
+DESTDIR="$PWD/temporary-destdir"
+
+curl --location --remote-name --skip-existing https://deb.debian.org/debian/pool/main/c/consolation/consolation_$VERSION.orig.tar.xz
+
+xz -cd consolation_$VERSION.orig.tar.xz | tar -x
+cd consolation-$VERSION
+
+./configure --prefix=/usr --sbindir=/usr/bin
+make
+make DESTDIR=$DESTDIR install-strip
+
+# man-pages require help2man
+rm -rf $DESTDIR/usr/share/man
+install -Dm644 LICENSE $DESTDIR/usr/share/LICENSES/consolation.license
+
+install -Dm755 ../consolation.run "$DESTDIR/etc/sv/consolation/run"
+ln -sf /run/runit/supervise.consolation "$DESTDIR/etc/sv/consolation/supervise"
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../consolation@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
