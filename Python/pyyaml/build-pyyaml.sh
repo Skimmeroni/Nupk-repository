18cc3711ab7c1ecc632987ef7625cd453ca113bb
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-19 09:27:45 +0000

    Added pyyaml

---
+++ Python/pyyaml/build-pyyaml.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+PRETTY_NAME=python:pyyaml
+MAJOR=6
+MINOR=0
+PATCH=2
+VERSION=6.0.2
+
+if [ ! -f $0 ]; then return; fi
+
+mkdir temporary-destdir
+DESTDIR="$PWD/temporary-destdir"
+
+curl --location --remote-name --skip-existing https://files.pythonhosted.org/packages/source/p/pyyaml/pyyaml-$VERSION.tar.gz
+
+gzip -cd pyyaml-$VERSION.tar.gz | tar -x
+cd pyyaml-$VERSION
+
+gpep517 install-from-source --optimize all --destdir $DESTDIR
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../pyyaml@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
