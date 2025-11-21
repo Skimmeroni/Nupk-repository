d3ea303a4886f9a379ccc07c6e881f488d3cdc9f
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-21 21:12:15 +0000

    Simplify building ca-certificates

--- Net/ca-certificates/build-ca-certificates.sh
+++ Net/ca-certificates/build-ca-certificates.sh
@@ -4,16 +4,15 @@ PRETTY_NAME=ca-certificates
 MAJOR=2025
 MINOR=11
 PATCH=04
-VERSION=2025-11-04
+VERSION=20251104
 
 if [ ! -f $0 ]; then return; fi
 
 mkdir temporary-destdir
 DESTDIR="$PWD/temporary-destdir"
 
-curl --location --remote-name --skip-existing https://curl.se/ca/cacert-$VERSION.pem
-
-install -Dm644 cacert-$VERSION.pem "$DESTDIR/etc/ssl/cert.pem"
+curl https://curl.se/ca/cacert-${MAJOR}-${MINOR}-${PATCH}.pem > cert.pem
+install -Dm644 cert.pem "$DESTDIR/etc/ssl/cert.pem"
 
 doas chown -R root:root $DESTDIR
 doas sh -c "tar -zcC $DESTDIR . | gzip > ca-certificates@$VERSION.tar.gz"
