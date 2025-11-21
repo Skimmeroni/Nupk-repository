56e45f71552e2f81800c5a34b59c4277dd4a2c3c
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-21 17:12:05 +0000

    Updated bc to 7.1.0

--- Devel/bc/build-bc.sh
+++ Devel/bc/build-bc.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -e
 
+# TODO: phase out in favour of Toybox bc
+
 PRETTY_NAME=bc-gh
 MAJOR=7
-MINOR=0
-PATCH=3
-VERSION=7.0.3
+MINOR=1
+PATCH=0
+VERSION=7.1.0
 
 if [ ! -f $0 ]; then return; fi
 
 mkdir temporary-destdir
 DESTDIR="$PWD/temporary-destdir"
 
-curl --location --remote-name --skip-existing https://github.com/gavinhoward/bc/releases/download//bc-$VERSION.tar.gz
+curl --location --remote-name --skip-existing https://github.com/gavinhoward/bc/releases/download/$VERSION/bc-$VERSION.tar.gz
 
 gzip -cd bc-$VERSION.tar.gz | tar -x
 cd bc-$VERSION
@@ -19,8 +21,8 @@ cd bc-$VERSION
 ./configure \
 	--prefix=/usr \
 	--enable-editline \
-	--disable-generated-tests \
 	--disable-nls \
+	--disable-generated-tests \
 	--disable-problematic-tests
 
 make
