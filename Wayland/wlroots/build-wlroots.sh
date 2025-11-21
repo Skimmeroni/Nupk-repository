303e945a74e80a4b1599f6a35d7957885ec44b96
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-21 21:13:20 +0000

    Updated wlroots to 0.19.2

--- Wayland/wlroots/build-wlroots.sh
+++ Wayland/wlroots/build-wlroots.sh
@@ -2,31 +2,36 @@
 
 PRETTY_NAME=wlroots
 MAJOR=0
-MINOR=17
-PATCH=3
-VERSION=0.17.3
+MINOR=19
+PATCH=2
+VERSION=0.19.2
 
 if [ ! -f $0 ]; then return; fi
 
 mkdir temporary-destdir
 DESTDIR="$PWD/temporary-destdir"
 
-curl --location --remote-name --skip-existing https://gitlab.freedesktop.org/wlroots/wlroots/-/archive//wlroots-$VERSION.tar.gz
+curl --location --remote-name --skip-existing https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/$VERSION/wlroots-$VERSION.tar.gz
 
 gzip -cd wlroots-$VERSION.tar.gz | tar -x
 cd wlroots-$VERSION
 
-meson setup \
-	-Dprefix=/usr \
-	-Ddefault_library=both \
-	-Dexamples=false \
-	-Dbackends=libinput,drm \
-	-Dxcb-errors=disabled \
-	-Drenderers=gles2 \
+# TODO: vulkan?
+muon setup \
+	-D prefix=/usr \
+	-D default_library=both \
+	-D backends='libinput,drm' \
+	-D renderers=gles2 \
+	-D color-management=enabled \
+	-D session=enabled \
+	-D xcb-errors=disabled \
+	-D xwayland=disabled \
+	-D libliftoff=disabled \
+	-D examples=false \
 	build
 
-meson -C build compile
-meson -C build install -d "$DESTDIR"
+ninja -C build
+muon -C build install -d "$DESTDIR"
 
 find "$DESTDIR/usr/lib" -type f -name '*.a'   -exec strip --strip-unneeded {} \;
 find "$DESTDIR/usr/lib" -type f -name '*.so*' -exec strip --strip-unneeded {} \;
