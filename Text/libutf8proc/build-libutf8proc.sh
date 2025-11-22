7ca6800702007c983f0ee7d0ae393ac79f248b4f
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-22 11:58:31 +0000

    Added libutf8proc

---
+++ Text/libutf8proc/build-libutf8proc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+PRETTY_NAME=libutf8proc
+MAJOR=2
+MINOR=11
+PATCH=1
+VERSION=2.11.1
+
+mkdir temporary-builddir
+DESTDIR="$PWD/temporary-builddir"
+
+curl --location --remote-name --skip-existing https://github.com/JuliaStrings/utf8proc/releases/download/v$VERSION/utf8proc-$VERSION.tar.gz
+
+gzip -cd utf8proc-$VERSION.tar.gz | tar -x
+cd utf8proc-$VERSION
+
+sed 's|prefix=/usr/local|prefix=/usr|g' Makefile > Makefile.new
+mv Makefile.new Makefile
+
+make
+make DESTDIR=$DESTDIR install
+
+find $DESTDIR -name '*.a'   -type f -exec strip --strip-unneeded {} \;
+find $DESTDIR -name '*.so*' -type f -exec strip --strip-unneeded {} \;
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../libutf8proc@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
