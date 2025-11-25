282b66966ae2480edf4933c566f6942f1c545455
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-25 12:22:48 +0000

    Added wayst

---
+++ Wayland/wayst/build-wayst.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+PRETTY_NAME=wayst
+MAJOR=
+MINOR=
+PATCH=
+VERSION=git
+
+mkdir temporary-builddir
+DESTDIR="$PWD/temporary-builddir"
+
+git clone https://github.com/91861/wayst.git
+cd wayst
+
+# Note: PGO and LTO are available
+make window_protocol=wayland renderer=gles20 libutf8proc=on mode=quick
+install -Dm755 wayst $DESTDIR/usr/bin/wayst
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-wayst@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
