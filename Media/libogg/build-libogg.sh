a0646c73cf7b290b8ccd8fe33b3cb5bc35c4676d
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-25 15:50:24 +0000

    Added libogg

---
+++ Media/libogg/build-libogg.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+
+PRETTY_NAME=libogg
+MAJOR=1
+MINOR=3
+PATCH=6
+VERSION=1.3.6
+
+if [ ! -f $0 ]; then return; fi
+
+DESTDIR="$PWD/temporary-destdir"
+[ -d $DESTDIR ] || mkdir temporary-destdir
+
+curl --location --remote-name --skip-existing https://github.com/xiph/ogg/releases/download/v$VERSION/libogg-$VERSION.tar.xz
+
+xz -cd libogg-$VERSION.tar.xz | tar -x
+cd libogg-$VERSION
+
+./configure \
+	--prefix=/usr \
+	--enable-shared \
+	--enable-static
+
+make -C src
+make DESTDIR=$DESTDIR -C include install
+make DESTDIR=$DESTDIR -C src install-strip
+make DESTDIR=$DESTDIR install-m4dataDATA
+make DESTDIR=$DESTDIR install-pkgconfigDATA
+
+find $DESTDIR -type f -name '*.la' -delete 
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Media-libogg@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
