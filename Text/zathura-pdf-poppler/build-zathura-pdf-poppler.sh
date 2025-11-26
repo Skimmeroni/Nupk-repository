744ed12094167079a344bb700105730924a318e4
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-26 21:13:52 +0000

    Added poppler

---
+++ Text/zathura-pdf-poppler/build-zathura-pdf-poppler.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+PRETTY_NAME=zathura-pdf-poppler
+MAJOR=0
+MINOR=3
+PATCH=4
+VERSION=0.3.4
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://github.com/pwmt/zathura-pdf-poppler/archive/$VERSION.tar.gz
+
+gzip -cd $VERSION.tar.gz | tar -x
+cd zathura-pdf-poppler-$VERSION
+
+muon setup \
+	-D prefix=/usr \
+	-D buildtype=release \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR
+
+strip --strip-unneeded $DESTDIR/usr/lib/zathura/libpdf-poppler.so
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Text-zathura-pdf-poppler@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
