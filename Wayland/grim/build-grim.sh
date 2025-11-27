5a53b94477d4ef8c0110d7a1a59566d1e6345ff7
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-27 10:23:59 +0000

    Added grim

---
+++ Wayland/grim/build-grim.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+
+PRETTY_NAME=grim
+MAJOR=1
+MINOR=5
+PATCH=0
+VERSION=1.5.0
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://gitlab.freedesktop.org/emersion/grim/-/releases/v$VERSION/downloads/grim-$VERSION.tar.gz
+
+gzip -cd grim-$VERSION.tar.gz | tar -x
+cd grim-$VERSION
+
+muon setup \
+	-D prefix=/usr \
+	-D buildtype=release \
+	-D bash-completions=false \
+	-D fish-completions=false \
+	-D jpeg=enabled \
+	-D man-pages=enabled \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR
+
+strip --strip-unneeded $DESTDIR/usr/bin/grim
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-grim@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
