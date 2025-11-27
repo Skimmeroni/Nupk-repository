dd186911fddf4be33c6c30aec0e8754cff4d868a
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-27 15:06:09 +0000

    Added bemenu

---
+++ Wayland/bemenu/build-bemenu.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+PRETTY_NAME=bemenu
+MAJOR=0
+MINOR=6
+PATCH=23
+VERSION=0.6.23
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://github.com/Cloudef/bemenu/releases/download/$VERSION/bemenu-$VERSION.tar.gz
+
+gzip -cd bemenu-$VERSION.tar.gz | tar -x
+cd bemenu-$VERSION
+
+make PREFIX=/usr clients wayland
+make PREFIX=/usr DESTDIR=$DESTDIR install
+
+strip --strip-unneeded $DESTDIR/usr/bin/bemenu
+strip --strip-unneeded $DESTDIR/usr/bin/bemenu-run
+find $DESTDIR -name '*.so*' -type f -exec strip --strip-unneeded {} \;
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-bemenu@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
