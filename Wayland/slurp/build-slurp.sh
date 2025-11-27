51b9912334f58bd92ee804cd25c6c6f38ecba5cd
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-27 10:24:09 +0000

    Added slurp

---
+++ Wayland/slurp/build-slurp.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+PRETTY_NAME=slurp
+MAJOR=1
+MINOR=5
+PATCH=0
+VERSION=1.5.0
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://github.com/emersion/slurp/releases/download/v$VERSION/slurp-$VERSION.tar.gz
+
+gzip -cd slurp-$VERSION.tar.gz | tar -x
+cd slurp-$VERSION
+
+muon setup \
+	-D prefix=/usr \
+	-D buildtype=release \
+	-D man-pages=enabled \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR
+
+strip --strip-unneeded $DESTDIR/usr/bin/slurp
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-slurp@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
