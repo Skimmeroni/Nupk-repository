c0604696fe36c361fe3cea362f7e5a4ac9448ca3
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-27 14:50:35 +0000

    Added wlsunset

---
+++ Wayland/wlsunset/build-wlsunset.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+
+PRETTY_NAME=wlsunset
+MAJOR=0
+MINOR=3
+PATCH=0
+VERSION=0.3.0
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://git.sr.ht/~kennylevinsen/wlsunset/archive/$VERSION.tar.gz
+
+gzip -cd $VERSION.tar.gz | tar -x
+cd wlsunset-$VERSION
+
+muon setup \
+	-D prefix=/usr \
+	-D buildtype=release \
+	-D man-pages=true \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR
+
+strip --strip-unneeded $DESTDIR/usr/bin/wlsunset
+install -Dm644 LICENSE "$DESTDIR/usr/share/LICENSES/wlsunset.license"
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-wlsunset@$VERSION.tar.gz"
+CALLER_UID=$(id -un)
+CALLER_GID=$(id -gn)
+doas chown -R $CALLER_UID:$CALLER_GID $DESTDIR
