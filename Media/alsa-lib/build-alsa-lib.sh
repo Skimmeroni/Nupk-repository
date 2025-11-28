b821bef15f343bb1c72dd609f280d046dfa8b8cc
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-28 23:24:27 +0100

    Added alsa-lib

---
+++ Media/alsa-lib/build-alsa-lib.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+
+PRETTY_NAME=alsa-lib
+MAJOR=1
+MINOR=2
+PATCH=14
+VERSION=1.2.14
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://www.alsa-project.org/files/pub/lib/alsa-lib-$VERSION.tar.bz2
+
+bzip2 -cd alsa-lib-$VERSION.tar.bz2 | tar -x
+cd alsa-lib-$VERSION
+
+# NOTE: it's not possible to compile both the static and the
+# dynamic libraries at the same time. It's one or the other
+./configure \
+	--prefix=/usr \
+	--enable-shared \
+	--disable-static \
+	--disable-python \
+	--without-debug
+
+make
+make DESTDIR=$DESTDIR install-strip
+
+find $DESTDIR -type f -name '*.la' -delete
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Media-alsa-lib@$VERSION.tar.gz"
+doas rm -rf $DESTDIR
