d88b406cce51bdb6d008befdcba867578bf35678
Federico Salvioni <federico.salvioni@tutanota.com>
2025-11-30 11:56:58 +0100

    Added gdk-pixbuf

---
+++ Wayland/gdk-pixbuf/build-gdk-pixbuf.sh
@@ -0,0 +1,52 @@
+#!/bin/sh -e
+
+PRETTY_NAME=gdk-pixbuf
+MAJOR=2
+MINOR=44
+PATCH=4
+VERSION=2.44.4
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://download.gnome.org/sources/gdk-pixbuf/$MAJOR.$MINOR/gdk-pixbuf-$VERSION.tar.xz
+ 
+xz -cd gdk-pixbuf-$VERSION.tar.xz | tar -x
+cd gdk-pixbuf-$VERSION
+
+# Disable preemptive caching in any shape or form
+print '#!/usr/bin/env python3' > build-aux/post-install.py
+patch -p1 < ../dont-test-loaders-cache.patch
+
+muon setup \
+	-D prefix=/usr \
+	-D buildtype=release \
+	-D default_library=both \
+	-D wrap_mode=nofallback \
+	-D png=enabled \
+	-D jpeg=enabled \
+	-D tiff=enabled \
+	-D gif=enabled \
+	-D builtin_loaders=all \
+	-D tests=false \
+	-D installed_tests=false \
+	-D documentation=false \
+	-D man=false \
+	-D gio_sniffing=false \
+	-D glycin=disabled \
+	-D introspection=disabled \
+	-D thumbnailer=disabled \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR 
+
+find "$DESTDIR/usr/lib" -type f -name '*.a'   -exec strip --strip-unneeded {} \;
+find "$DESTDIR/usr/lib" -type f -name '*.so*' -exec strip --strip-unneeded {} \;
+
+doas chown -R root:root $DESTDIR
+doas sh -c "tar -zcC $DESTDIR . | gzip > ../Wayland-gdk-pixbuf@$VERSION.tar.gz"
+doas rm -rf $DESTDIR
+
+printf "\033[1mRemember to generate the loaders cache!\033[0m\n"
+# doas gdk-pixbuf-query-loaders
