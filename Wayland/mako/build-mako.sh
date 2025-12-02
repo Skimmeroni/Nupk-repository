dde2cc29c75197ca28f301c025a1b0c01e7e39fd
Federico Salvioni <federico.salvioni@tutanota.com>
2025-12-02 15:42:39 +0100

    Added mako

---
+++ Wayland/mako/build-mako.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -eu
+
+PRETTY_NAME=mako
+MAJOR=1
+MINOR=10
+PATCH=0
+VERSION=1.10.0
+
+DESTDIR="$PWD/temporary-destdir"
+[ -d $DESTDIR ] || mkdir -p $DESTDIR
+
+curl --location --remote-name --skip-existing https://github.com/emersion/mako/releases/download/v$VERSION/mako-$VERSION.tar.gz
+
+gzip -cd mako-$VERSION.tar.gz | tar -x
+cd mako-$VERSION
+
+muon setup \
+	-D prefix=/usr \
+	-D default_library=both \
+	-D buildtype=release \
+	-D wrap_mode=nofallback \
+	-D sd-bus-provider=basu \
+	-D man-pages=enabled \
+	-D icons=enabled \
+	build
+
+ninja -C build
+muon -C build install -d $DESTDIR
+
+strip --strip-unneeded "$DESTDIR/usr/bin/mako"
+strip --strip-unneeded "$DESTDIR/usr/bin/makoctl"
+
+doas chown -R root:root $DESTDIR
+cd $DESTDIR
+doas sh -c "tar -cf - * | gzip > ../Wayland-mako@$VERSION.tar.gz"
+doas rm -rf $DESTDIR
