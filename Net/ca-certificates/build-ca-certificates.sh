b047bd16f62a802330c7da03eefa1e3d1eb77535
Federico Salvioni <federico.salvioni@tutanota.com>
2025-12-03 20:27:08 +0100

    Updated ca-certificates to 20251202

--- Net/ca-certificates/build-ca-certificates.sh
+++ Net/ca-certificates/build-ca-certificates.sh
@@ -4,9 +4,9 @@ set -eu
 
 PRETTY_NAME=ca-certificates
 MAJOR=2025
-MINOR=11
-PATCH=04
-VERSION=20251104
+MINOR=12
+PATCH=02
+VERSION=20251202
 
 DESTDIR="$PWD/temporary-destdir"
 [ -d $DESTDIR ] || mkdir -p $DESTDIR
@@ -16,5 +16,5 @@ install -Dm644 cert.pem "$DESTDIR/etc/ssl/cert.pem"
 
 doas chown -R root:root $DESTDIR
 cd $DESTDIR
-doas sh -c "tar -cf - * | gzip > Net-ca-certificates@$VERSION.tar.gz"
+doas sh -c "tar -cf - * | gzip > ../Net-ca-certificates@$VERSION.tar.gz"
 doas rm -rf $DESTDIR
