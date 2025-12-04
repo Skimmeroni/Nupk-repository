e484ec230946007e649d2c9c3ae0e47e365d2dce
Federico Salvioni <federico.salvioni@tutanota.com>
2025-12-04 17:48:47 +0100

    Added alsa-ucm-conf

---
+++ Media/alsa-ucm-conf/build-alsa-ucm-conf.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eu
+
+PRETTY_NAME=alsa-ucm-conf
+MAJOR=1
+MINOR=2
+PATCH=14
+VERSION=1.2.14
+
+DESTDIR="$PWD/temporary-builddir"
+[ -d $DESTDIR ] || mkdir temporary-builddir
+
+curl --location --remote-name --skip-existing https://www.alsa-project.org/files/pub/lib/alsa-ucm-conf-$VERSION.tar.bz2
+
+bzip2 -cd alsa-ucm-conf-$VERSION.tar.bz2 | tar -x
+cd alsa-ucm-conf-$VERSION
+
+mkdir -p "$DESTDIR/usr/share/alsa"
+cp -r ucm "$DESTDIR/usr/share/alsa"
+cp -r ucm2 "$DESTDIR/usr/share/alsa"
+
+doas chown -R root:root $DESTDIR
+cd $DESTDIR
+doas sh -c "tar -cf - * | gzip > ../Media-alsa-ucm-conf@$VERSION.tar.gz"
+doas rm -rf $DESTDIR
