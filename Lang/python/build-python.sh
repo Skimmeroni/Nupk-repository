5447bc444318985b0881bdba66c6c9a401c8bf9e
Federico Salvioni <federico.salvioni@tutanota.com>
2025-12-06 16:19:29 +0100

    Versioned libressl patch for python

--- Lang/python/build-python.sh
+++ Lang/python/build-python.sh
@@ -5,19 +5,19 @@ set -eu
 PRETTY_NAME=python
 MAJOR=3
 MINOR=14
-PATCH=1
-VERSION=3.14.1
+PATCH=0
+VERSION=3.14.0
 
 DESTDIR="$PWD/temporary-destdir"
 [ -d $DESTDIR ] || mkdir -p $DESTDIR
 
 curl --location --remote-name --skip-existing https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tar.xz
-curl --location --remote-name --skip-existing https://raw.githubusercontent.com/gentoo/libressl/refs/heads/master/dev-lang/python/files/python-3.14.0-libressl.patch
+curl --location --remote-name --skip-existing https://raw.githubusercontent.com/gentoo/libressl/refs/heads/master/dev-lang/python/files/python-$VERSION-libressl.patch
 
-tar -xf Python-$VERSION.tar.xz
+xz -cd Python-$VERSION.tar.xz | tar -x
 cd Python-$VERSION
 
-patch -p1 < ../python-3.14.0-libressl.patch
+patch -p1 < ../python-$VERSION-libressl.patch
 
 cat >> Modules/Setup << EOF
 *disabled*
