85882ab789a36bbc55b3fd362430303a9cb77896
Federico Salvioni <federico.salvioni@tutanota.com>
2025-12-08 01:07:36 +0100

    Added numpy

---
+++ Python/numpy/build-numpy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+PRETTY_NAME=python:numpy
+MAJOR=2
+MINOR=3
+PATCH=5
+VERSION=2.3.5
+
+DESTDIR="$PWD/temporary-destdir"
+[ -d $DESTDIR ] || mkdir -p $DESTDIR
+
+curl --location --remote-name --skip-existing https://github.com/numpy/numpy/releases/download/v$VERSION/numpy-$VERSION.tar.gz
+
+gzip -cd numpy-$VERSION.tar.gz | tar -x
+cd numpy-$VERSION
+
+gpep517 install-from-source --optimize all --destdir $DESTDIR
+
+find $DESTDIR -name '*.a'   -type f -exec strip --strip-unneeded {} \;
+find $DESTDIR -name '*.so*' -type f -exec strip --strip-unneeded {} \;
+
+doas chown -R root:root $DESTDIR
+cd $DESTDIR
+doas sh -c "tar -cf - * | gzip > ../Python-numpy@$VERSION.tar.gz"
+doas rm -rf $DESTDIR
